aece0d89c72ae6a1601fafd336683f7316a77e17
Лидия Пятаева <z_liya@inbox.ru>
2024-06-14 12:14:42 +0500

    Initial Commit

---
+++ RestaurantProject/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  RestaurantProject
+//
+//  Created by Лидия Пятаева on 14.06.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
